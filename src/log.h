7b960d9abac993fe13d59ceb7f0a42bb76493559
Casey Williams <williamshoops96@gmail.com>
2020-05-23 18:49:22 +0100

    Make log ISO compliant

--- src/log.h
+++ src/log.h
@@ -5,15 +5,15 @@ enum { LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
 #ifndef NLOG
 
-#define log_debug(...) log_log(LOG_DEBUG, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(LOG_DEBUG, __func__, __FILE__, __LINE__, __VA_ARGS__)
 
-#define log_info(...) log_log(LOG_INFO, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+#define log_info(...) log_log(LOG_INFO, __func__, __FILE__, __LINE__, __VA_ARGS__)
 
-#define log_warn(...) log_log(LOG_WARN, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+#define log_warn(...) log_log(LOG_WARN, __func__, __FILE__, __LINE__, __VA_ARGS__)
 
-#define log_error(...) log_log(LOG_ERROR, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+#define log_error(...) log_log(LOG_ERROR, __func__, __FILE__, __LINE__, __VA_ARGS__)
 
-#define log_fatal(...) log_log(LOG_FATAL, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+#define log_fatal(...) log_log(LOG_FATAL, __func__, __FILE__, __LINE__, __VA_ARGS__)
 
 #else
 
